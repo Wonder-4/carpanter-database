22f30e36dd14acbb27847c6ab7882958da515bc5
黃翰隆 <issac11235813@gmail.com>
2016-05-19 10:36:31 +0800

    carpanter datastore
    python for google cloud platform

--- carpanter-database/main.py
+++ carpanter-database/main.py
@@ -14,23 +14,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
 
+# -*- coding: utf-8 -*- 
+import webapp2
+from google.appengine.ext import ndb
+import cgi
+import urllib
+
+class Greeting(ndb.Model):
+    """Models an individual Guestbook entry with content and date."""
+    content = ndb.StringProperty()
+    def query_book(cls, ancestor_key):
+        return cls.query(ancestor=ancestor_key).order(-cls.date)
 #Read Data
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.out.write('<html><body>')
+        guestbook_name = self.request.get('guestbook_name')
+        ancestor_key = ndb.Key("Book", guestbook_name or "*notitle*")
+        greetings = Greeting.query_book(ancestor_key).fetch(20)
+
+        for greeting in greetings:
+            self.response.out.write('<blockquote>%s</blockquote>' %
+                                    cgi.escape(greeting.content))
 #Insert Data
 class Create(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Create')
+    def post(self):
+        # We set the parent key on each 'Greeting' to ensure each guestbook's
+        # greetings are in the same entity group.
+        guestbook_name = self.request.get('guestbook_name')
+        test = urllib.unquote(guestbook_name.encode('ascii')).decode('utf-8')
+        print '<p>%s</p>'%test
+        content=self.request.get('content')
+        con = urllib.unquote(content.encode('ascii')).decode('utf-8')
+        print '<p>%s</p>'%con
+        #try:
+        #    greeting = Greeting(parent=ndb.Key("Book",test ),con)
+        #    greeting.put()
+        #    self.redirect('/')
+        #except e :
+        #    print '<p>%s</p><p>%s</p>'%(test,con)
+
 #Update data
 class Update(webapp2.RequestHandler):
-    def get(self):
+    def post(self):
         self.response.write('Update')
 #Delete data
 class Delete(webapp2.RequestHandler):
-    def get(self):
+    def post(self):
         self.response.write('Delete')
 
 app = webapp2.WSGIApplication([
